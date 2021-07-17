ae148803856071978bd4d9b7ab1f8f394a1f11f7
Jonas Torquatro <60902980+CipherCrow@users.noreply.github.com>
2021-07-16 22:54:00 -0300

    :up: responsivo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-let mob = $('nav.mobile i')
-mob.click(function(){
+let mob = $('nav.mobile i');
+let linksmobile = $('nav.mobile ul li a');
+let abrirFechar = function(){
     $('nav.mobile ul').slideToggle();
     mob.toggleClass(["fa-bras","fa-times"]);
-})
+};
+mob.click(abrirFechar);
+linksmobile.click(abrirFechar);
